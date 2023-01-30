fd1d1c9e1192e69f21e8190aa1b6a69773184cf7
aheldis <ehsanihani30@yahoo.com>
2023-01-30 09:03:58 +0000

    debug

--- cmd_args.py
+++ cmd_args.py
@@ -58,7 +58,7 @@ def postprocess(args):
 
 def parse_args_from_yaml(yaml_path):
     with open(yaml_path, 'r') as fd:
-        args = yaml.load(fd)
+        args = yaml.full_load(fd)
         args = EasyDict(d=args)
         args = postprocess(args)
     return args
